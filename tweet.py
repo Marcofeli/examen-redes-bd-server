238779a9677e5dc17375b6529f6d9b2455e10d36
Felipe <f.1419@hotmail.com>
2018-06-13 10:09:56 -0600

    Email sending done

--- tweet.py
+++ tweet.py
@@ -1,6 +1,9 @@
 import tweepy
 import mysql.connector
 import threading
+import smtplib
+from email.message import EmailMessage
+import time
 
 # Autentificacion
 consumer_key = 'Vwx4h6GxWXva3SNnndSlETE17'
@@ -22,37 +25,63 @@ cursor = cnx.cursor()
 
 # Lista de tweets
 tweets = list()
-query = "Select id from tweet"
+query = "Select id,text from tweet"
 cursor.execute(query)
-for (id) in cursor:
-    tweets.append(id[0])
+for t in cursor:
+    tweets.append((t[0],t[1]))
 
+# valores usados para enviar correo
+FROM = 'empalidandoconrojo@gmail.com'
+TO = 'rojo@tec.ac.cr' #rojo@tec.ac.cr
+msg = 'Se han publicado los siguientes tweets: \n {} \n PD: que examen más rudo'
+content = ""
 
 def retrieveData():
+    global content
+    global msg
     while True:
         # Buscar tweets
         public_tweets = api.search('#intentadoSalvarElSemestreConRojo')
         # insertar tweets
         for tweet in public_tweets:
-            id = tweet.id_str
-            if id in tweets:
+            id = tweet._json["id_str"]
+            date = tweet._json["created_at"]
+            text = tweet._json["text"]
+            user = tweet._json["user"]["screen_name"]
+            add_tweet = "INSERT INTO tweet (date, text, user, id) VALUES ('{}','{}','{}','{}')".format(date,text,user,id)
+            try:
+                cursor.execute(add_tweet)
+                cnx.commit()
+            except:
                 continue
-            date = tweet.created_at
-            text = tweet.text
-            add_tweet = "INSERT INTO tweet (date, text, user, id) VALUES ('{}','{}','{}','{}')".format(date,text,tweet.user._json["screen_name"],tweet.id_str)
-            cursor.execute(add_tweet)
-            cnx.commit()
-            tweets.append(id)
+            tweets.append((id, text))
             if len(tweets)%10 == 0:
-                print(len(tweets))
                 print("Llegó a 10 tweets nuevos, enviar correo")
+                for i in range(len(tweets)-10,len(tweets)):
+                    content += str(i+1)+") "+tweets[i][1] + "\n"
+                #Envia correo
+                emsg = EmailMessage()
+                emsg.set_content('lorem ipsum dolor amet')
+                emsg['Subject'] = 'Ola ke ase, empalidando o ke ase'
+                emsg['From'] = FROM
+                emsg['To'] = TO
 
-def main():
-    t = threading.Thread(target=retrieveData)
-    t.start()
+                server = smtplib.SMTP('smtp.gmail.com', 587)
+                server.starttls()
+                server.login("empalidandoconrojo@gmail.com", "#intentadoSalvarElSemestreConRojo")
+
+                msg = msg.format(content)
+                print(msg)
+
+                server.sendmail(emsg['From'], emsg['To'], msg.encode())
+                server.quit()
+
+        time.sleep(60*5) #cada 5 minutos se repite el proceso
+
+
+t = threading.Thread(target=retrieveData)
+t.start()
 
 #Cierra la conexión
 # cursor.close()
 # cnx.close()
-
-main()
